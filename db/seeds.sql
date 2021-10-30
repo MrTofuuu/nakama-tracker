666a7debb60a5c03aca8e8dd142384527de9c3a3
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-10-30 00:25:24 -0500

    made some errors on sql join for all

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,25 +5,25 @@ VALUES  ('Hero'),
         ('SAITAMA');
 
 INSERT INTO roles (title, salary, departments_id)
-VALUES  ('Pirate',99999,1),
-        ('Ninja',50000,2),
-        ('Mecha',25000,3),
-        ('Demon Slayer',60000,1),
-        ('Saiyan',75000,2),
-        ('Samurais',80000,3),
-        ('Space Cowboys',15000,3),
-        ('Kaiju',69000,1),
-        ('Bored Bald Person',100,4);
+VALUES  ('Pirate',99999.99,1),
+        ('Ninja',50000.00,2),
+        ('Mecha',25000.00,3),
+        ('Demon Slayer',60000.00,1),
+        ('Saiyan',75000.00,2),
+        ('Samurais',80000.00,3),
+        ('Space Cowboys',15000.00,3),
+        ('Kaiju',69000.00,1),
+        ('Bored Bald Person',100.00,4);
 
 INSERT INTO employees (first_name,last_name,roles_id,manager_id)
-VALUES  ('Goku', 'Son',2,null),
-        ('Vegeta','The Fourth',2,1),
+VALUES  ('Goku', 'Son',5,null),
+        ('Vegeta','The Fourth',5,1),
         ('Luffy','Monkey D.',1,null),
         ('Zoro','Roronoa',1 ,3 ),
         ('Sanji','Vinsmoke',1 ,3 ),
-        ('Gohan','Son',2 , 1),
-        ('Kafka','Hibino',8 , null),
+        ('Gohan','Son',5 , 1),
         ('Tanjiro','Kamado',4 ,null ),
+        ('Kafka','Hibino',8 , null),
         ('Nezuko','Kamado',4 ,9 ),
         ('Zenitsu','Agatsuma',4 ,9 ),
         ('Spike','Spiegel',7 ,null ),
--- helpers/dbUtils.js
+++ helpers/dbUtils.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection(
 );
 
 const viewDepartments = () => {
-  let query = "SELECT * FROM departments";
+  let query = "SELECT * FROM departments;";
   db.query(query, (err, results) => {
     if (err) {
       return "error in viewDepartments query";
@@ -22,7 +22,7 @@ const viewDepartments = () => {
 };
 
 const viewRoles = () => {
-  let query = "SELECT * FROM roles";
+  let query = "SELECT departments.name, roles.roles_id, roles.title, roles.salary FROM roles JOIN departments ON roles.departments_id = departments.departments_id;";
   db.query(query, (err, results) => {
     if (err) {
       return "error in viewRoles query";
@@ -32,7 +32,8 @@ const viewRoles = () => {
 };
 
 const viewEmployees = () => {
-  let query = "SELECT * FROM employees";
+  let query = "SELECT  e.employees_id AS 'Employee ID', e.first_name AS 'First Name', e.last_name AS 'Last Name',d.name AS 'Department', r.title AS 'Title', r.salary AS 'Salary' FROM employees as e INNER JOIN roles as r ON e.roles_id = r.roles_id INNER JOIN departments as d ON r.departments_id = d.departments_id;";
+  "select e.employees_id, e.first_name, e.last_name, m.first_name as 'manager first name',m.last_name as 'manager last name' FROM employees as e join employees as m ON e.employees_id = m.employees_id;"
   db.query(query, (err, results) => {
     if (err) {
       return "error in viewEmployees query";
@@ -42,7 +43,7 @@ const viewEmployees = () => {
 };
 
 const addDepartment = (dept) => {
-  let query = "INSERT INTO departments (name) VALUES (?)";
+  let query = "INSERT INTO departments (name) VALUES (?);";
   let params = [dept];
   db.query(query, params, (err, results) => {
     if (err) {
@@ -53,7 +54,7 @@ const addDepartment = (dept) => {
 };
 
 const addRole = (title, salary, deptartment_id) => {
-  let query = "INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)";
+  let query = "INSERT INTO roles (title, salary, department_id) VALUES (?,?,?);";
   db.query(query, params, (err, results) => {
     if (err) {
       return "error in addRole query";
@@ -63,7 +64,8 @@ const addRole = (title, salary, deptartment_id) => {
 };
 
 const addEmployee = (first, last, role_id, manager_id) => {
-  let query = "INSERT INTO employees (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?)";
+  let query =
+    "INSERT INTO employees (first_name,last_name,role_id,manager_id) VALUES (?,?,?,?);";
   db.query(query, params, (err, results) => {
     if (err) {
       return "error in addEmployee query";
@@ -73,7 +75,7 @@ const addEmployee = (first, last, role_id, manager_id) => {
 };
 
 const updateEmployeeRole = (newRole, currentRole) => {
-  let query = "UPDATE employees SET role_id = ? WHERE id = ?";
+  let query = "UPDATE employees SET role_id = ? WHERE id = ?;";
   db.query(query, params, (err, results) => {
     if (err) {
       return "error in updateEmployeeRole query";
